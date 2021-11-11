0fa5e58e12cb82ccf0fc43427419e7a730789f76
0xPlayPad <89713202+0xPlayPad@users.noreply.github.com>
2021-11-11 22:39:10 +0300

    missings fixed which are provided by peckshield

--- playPadIdoMain.sol
+++ playPadIdoMain.sol
@@ -781,25 +781,22 @@ contract MainPlayPadContract is Ownable, ApproverRole, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public stakingToken; //staking token
-    IERC20 public rewardToken; // reward token contract address for investors
+    IERC20 public immutable stakingToken; //staking token
+    IERC20 public immutable rewardToken; // reward token contract address for investors
     uint256 public startBlock; // start block number to decide vestings and distrubute prizes 
     uint256 public lastRewardBlock; //last reward block to distrubute reward tokens.
-    uint256 public finishBlock; //Finish block number for staking deposits.
+    uint256 public immutable finishBlock; //Finish block number for staking deposits.
     uint256 public allStakedAmount; //total staked value as tokens.
     uint256 public allPaidReward; //total distrubuted tokens value.
     uint256 public allRewardDebt; //total pending reward value.
-    uint256 public poolTokenAmount; //total prize to be distrubuted for period.
-    uint256 public rewardPerBlock; //reward value per block.
+    uint256 public immutable poolTokenAmount; //total prize to be distrubuted for period.
+    uint256 public immutable rewardPerBlock; //reward value per block.
     uint256 public accTokensPerShare; // Accumulated tokens per share.
     uint256 public participants; //Count of participants.
-    bool public isPenalty; //penalty status for each deposit.
-    uint256 public penaltyRate; //penaltyRate for pool.
-    uint256 public penaltyBlockLength; //penalty block duration as block number.
-    address public penaltyAddress; //Penalty address.
-    uint256 public stakeStartDate; //first date of deposit of user above limit for vesting.
-    bool public stakeStatus; //User stake status.
-    bool public onlyPrize; //Only prize, No vesting.
+    bool public immutable isPenalty; //penalty status for each deposit.
+    uint256 public immutable penaltyRate; //penaltyRate for pool.
+    uint256 public immutable penaltyBlockLength; //penalty block duration as block number.
+    address public immutable penaltyAddress; //Penalty address.
     uint256 public limitForPrize; // limit token count to get vesting from IDOs.
     address[] public newIdo; // All deployed IDO addresses.
     address[] public allInvestors; //return all investor
@@ -837,6 +834,7 @@ contract MainPlayPadContract is Ownable, ApproverRole, ReentrancyGuard {
     event WithdrawAllPools(address indexed user, uint256 amount);
     event WithdrawPoolRemainder(address indexed user, uint256 amount);
     event NewIdoCreated(address indexed idoUser);
+    event ChangePrizeLimit(uint256 amount);
     
     constructor(
         IERC20 _stakingToken,
@@ -976,39 +974,11 @@ contract MainPlayPadContract is Ownable, ApproverRole, ReentrancyGuard {
     }
     
     
-       // creates new IDO contract following datas as below
-       function createIDO(
-        IERC20 _busdAddress,
-        IERC20 _saleToken,
-        bool _contractStatus,
-        uint256 _hardcapUsd,
-        uint256 _totalSellAmountToken,
-        uint256 _maxInvestorCount,
-        uint256 _maxBuyValue,
-        uint256 _minBuyValue,
-        uint256 _startTime,
-        uint256 _endTime
-    ) external nonReentrant onlyApprover {
-         PlayPadIdoContract newIdoContract = new PlayPadIdoContract(
-            _busdAddress,
-            _saleToken,
-            _contractStatus,
-            _hardcapUsd,
-            _totalSellAmountToken,
-            _maxInvestorCount,
-            _maxBuyValue,
-            _minBuyValue,
-            _startTime,
-            _endTime
-        );
-        newIdo.push(address(newIdoContract)); // Adding All IDOs
-        newIdoContract.transferOwnership(msg.sender);
-        isIdoDeployed[address(newIdoContract)] = true;
-        emit NewIdoCreated(address(newIdoContract));
-    }
+      
     
     function changePrizeLimit(uint256 _amountToStake) external nonReentrant onlyApprover {
         limitForPrize = _amountToStake;
+        emit ChangePrizeLimit(limitForPrize);
     }
     
      function getUserInfo(address _user)
@@ -1069,7 +1039,7 @@ contract MainPlayPadContract is Ownable, ApproverRole, ReentrancyGuard {
                 penaltyBlockLength
             );
             
-            if(user.amount.add(_amountToStake) > limitForPrize){
+            if(user.amount.add(_amountToStake) > limitForPrize && user.stakeStartDate != 0){
                 user.onlyPrize = false;
                 user.stakeStartDate = block.timestamp;
             }else{
@@ -1100,6 +1070,7 @@ contract MainPlayPadContract is Ownable, ApproverRole, ReentrancyGuard {
         require(user.amount >= _amount, "withdraw: not good");
         require(poolInfo.amount >= _amount, "withdraw: not good");
         require(poolInfo.owner == msg.sender, "you are not owner");
+        require(block.number > finishBlock, "date is not good");
         updatePool();
         uint256 pending = transferPendingReward(user);
         uint256 penaltyAmount = 0;
@@ -1112,7 +1083,7 @@ contract MainPlayPadContract is Ownable, ApproverRole, ReentrancyGuard {
                 if (block.number < finishBlock) {
                     if (block.number <= poolInfo.penaltyEndBlockNumber) {
                         penaltyAmount = penaltyRate.mul(_amount).div(1e6);
-                        stakingToken.transfer(penaltyAddress, penaltyAmount);
+                        stakingToken.safeTransferFrom(msg.sender, penaltyAddress, penaltyAmount);
                     }
                 }
             }
@@ -1171,204 +1142,3 @@ contract MainPlayPadContract is Ownable, ApproverRole, ReentrancyGuard {
     }
 }
 
-
-
-
-
-
-
-
-
-contract PlayPadIdoContract is ReentrancyGuard, Ownable {
-   
-    //Deployed by Main Contract
-    MainPlayPadContract deployerContract;
-    using SafeERC20 for IERC20;
-    using SafeMath for uint256;
-
-      IERC20 public busdToken; //Stable coin token contract address
-    IERC20 public saleToken; //Sale token contract address
-    uint256 public hardcapUsd; //hardcap value as usd 
-    uint256 public totalSellAmountToken; //total amount to be sold
-    uint256 public maxInvestorCount; //max. investor count
-    uint256 public maxBuyValue; //max buying value per investor
-    uint256 public minBuyValue; //min buying value per investor
-    bool public contractStatus; //Contract running status
-    uint256 public startTime; //IDO participation start time
-    uint256 public endTime; //IDO participation end time
-    uint256 public totalSoldAmountToken; //total sold amount as token
-    uint256 public totalSoldAmountUsd; // total sold amount as usd
-    uint256 public lockTime; //unlock date to get claim 
-    address[] public whitelistedAddresses; //whitelisted address as array
-    uint256[] public claimRoundsDate; //all claim rounds
-    
-   //whitelisted user data per user
-    struct whitelistedInvestorData {
-        uint256 totalBuyingAmountUsd;
-        uint256 totalBuyingAmountToken;
-        uint claimRound;
-        bool isWhitelisted;
-        uint256 lastClaimDate;
-        uint256 claimedValue;
-        address investorAddress;
-        uint256 totalVesting;
-        bool iWillBuy;
-    }
-    //claim round periods
-    struct roundDatas {
-        uint256 roundStartDate;
-        uint256 roundPercent;
-    }
-    //mappings to reach relevant information
-    mapping(address => whitelistedInvestorData) public _investorData;
-    mapping(uint256 => roundDatas) public _roundDatas;
-
-    
-    
-   
-    constructor(
-        IERC20 _busdAddress,
-        IERC20 _saleToken,
-        bool _contractStatus,
-        uint256 _hardcapUsd,
-        uint256 _totalSellAmountToken,
-        uint256 _maxInvestorCount,
-        uint256 _maxBuyValue,
-        uint256 _minBuyValue,
-        uint256 _startTime,
-        uint256 _endTime
-    ) public {
-        require(
-            _startTime < _endTime,
-            "start block must be less than finish block"
-        );
-        require(
-            _endTime > block.timestamp,
-            "finish block must be more than current block"
-        );
-        busdToken = _busdAddress;
-        saleToken = _saleToken;
-        contractStatus = _contractStatus;
-        hardcapUsd = _hardcapUsd;
-        totalSellAmountToken = _totalSellAmountToken;
-        maxInvestorCount = _maxInvestorCount;
-        maxBuyValue = _maxBuyValue;
-        minBuyValue= _minBuyValue;
-        startTime = _startTime;
-        endTime = _endTime;
-    }
-      
-    event NewBuying(address indexed investorAddress, uint256 amount, uint256 timestamp);
-    
-    //modifier to change contract status
-    modifier mustNotPaused() {
-        require(!contractStatus, "Paused!");
-        _;
-    }
-    
-    // function to change status of contract
-    function changePause(bool _contractStatus) public onlyOwner nonReentrant{
-        contractStatus = _contractStatus;
-    }
-    
-     function changeSaleTokenAddress(IERC20 _contractAddress) external onlyOwner nonReentrant {
-        saleToken = _contractAddress;
-    } 
-    
-    //return all whitelisted addresses as array
-    function getWhitelistedAddresses() public view returns(address[] memory){
-        return whitelistedAddresses;
-    }
-    
-    //calculate token amount according to deposit amount
-    function calculateTokenAmount(uint256 amount) public view returns (uint256) {
-       return (totalSellAmountToken.mul(amount)).div(hardcapUsd);
-    }
-    
-    function returnUserInfo(address _addresss) public view returns (uint256, uint256, uint, bool, uint256, uint256, address, uint256, bool){
-        whitelistedInvestorData storage investor = _investorData[msg.sender];
-        return (investor.totalBuyingAmountUsd, investor.totalBuyingAmountToken, investor.claimRound, investor.isWhitelisted, investor.lastClaimDate, investor.claimedValue, investor.investorAddress, investor.totalVesting, investor.iWillBuy);
-    }
-    
-      
-    
-    //buys token if passing controls
-    function buyToken(uint256 busdAmount) external nonReentrant mustNotPaused {
-        require(block.timestamp >= startTime);
-        require(block.timestamp <= endTime);
-        whitelistedInvestorData storage investor = _investorData[msg.sender];
-        require(investor.isWhitelisted);
-        require(busdAmount >= minBuyValue);
-        require(maxBuyValue >= investor.totalBuyingAmountUsd.add(busdAmount));
-        require(busdToken.transferFrom(msg.sender, address(this), busdAmount));
-        uint256 totalTokenAmount = calculateTokenAmount(busdAmount);
-        investor.totalBuyingAmountUsd = investor.totalBuyingAmountUsd.add(busdAmount);
-        investor.totalBuyingAmountToken = investor.totalBuyingAmountToken.add(totalTokenAmount);
-        totalSoldAmountToken = totalSoldAmountToken.add(totalTokenAmount);
-        totalSoldAmountUsd = totalSoldAmountUsd.add(busdAmount);
-        emit NewBuying(msg.sender, busdAmount, block.timestamp);
-    }
-    
-    //emergency withdraw function in worst cases
-    function emergencyWithdrawAllBusd() external nonReentrant onlyOwner {
-        require(busdToken.transferFrom(address(this), msg.sender, busdToken.balanceOf(address(this))));
-    }
-    //change lock time to prevent missing values
-    function changeLockTime(uint256 _lockTime) external nonReentrant onlyOwner {
-        lockTime = _lockTime;
-    }
-    //emergency withdraw for tokens in worst cases
-     function withdrawTokens() external nonReentrant onlyOwner {
-        require(saleToken.transfer(msg.sender, saleToken.balanceOf(address(this))));
-    }
-    
-    // Change iWillBuy
-    function iWillBuy(bool _value) external nonReentrant {
-         whitelistedInvestorData storage investor = _investorData[msg.sender];
-         investor.iWillBuy = _value;
-    }
-    
-    //claim tokens according to claim periods 
-     function claimTokens() external nonReentrant {
-        require(block.timestamp >= lockTime, "bad lock time");
-        whitelistedInvestorData storage investor = _investorData[msg.sender];
-        require(investor.isWhitelisted, "you are not whitelisted");
-        uint256 investorRoundNumber = investor.claimRound;
-        require(roundDetail.roundStartDate != 0, "Claim rounds are not available yet.");
-        roundDatas storage roundDetail = _roundDatas[investorRoundNumber];
-        require(block.timestamp >= roundDetail.roundStartDate ,"round didn't start yet");
-        require(investor.totalBuyingAmountToken >= investor.claimedValue.add(investor.totalBuyingAmountToken.mul(roundDetail.roundPercent).div(100)) ,"already you got all your tokens");
-        require(saleToken.transfer(msg.sender, investor.totalBuyingAmountToken.mul(roundDetail.roundPercent).div(100)) ,"bad transfer");
-        investor.claimRound = investor.claimRound.add(1);
-        investor.lastClaimDate = block.timestamp;
-        investor.claimedValue = investor.claimedValue.add(investor.totalBuyingAmountToken.mul(roundDetail.roundPercent).div(100));
-    }
-     
-    //add new claim round
-    function addNewClaimRound(uint256 _roundNumber, uint256 _roundStartDate, uint256 _claimPercent) external nonReentrant onlyOwner {
-    roundDatas storage roundDetail = _roundDatas[_roundNumber];
-    roundDetail.roundStartDate = _roundStartDate;
-    roundDetail.roundPercent = _claimPercent;
-    claimRoundsDate.push(_roundStartDate);
-    }
-    
-   function changeMaxMinBuyLimit(uint256 _maxBuyLimit, uint256 _minBuyLimit) external onlyOwner nonReentrant {
-       maxBuyValue = _maxBuyLimit;
-       minBuyValue = _minBuyLimit;
-   }
-   
-    /*
-    Define users vestings and addresses according to datas taken from javascript. function at javascript controls buy limits and other details
-    after that it multiples stake duration time of users with their deposited amounts and converts them to percentage based result and calculate their buying amounts according to tokenomics of IDO
-    */
-    
-    function addUsersToWhitelist(address[] memory _whitelistedAddresses, uint256[] memory _buyingLimits) external onlyOwner nonReentrant{ 
-        for(uint256 i = 0;  i < _whitelistedAddresses.length; i++){
-             whitelistedInvestorData storage investor = _investorData[_whitelistedAddresses[i]];
-             investor.totalVesting = _buyingLimits[i];
-             investor.isWhitelisted = true;
-             whitelistedAddresses.push(_whitelistedAddresses[i]);
-        }
-       
-    }
- }
